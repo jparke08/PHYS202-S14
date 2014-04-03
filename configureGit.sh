bb05b61ecdafb475f20293e416296c5b954019e0
Joshua Parker <jparker291@gmail.com>
2014-04-03 13:05:50 -0700

    added a documentation

---
+++ configureGit.sh
@@ -0,0 +1,9 @@
+#t/bin/bash
+
+# This script configures git whenever you start working 
+#on multiple machines. Run it any time you decitde to work
+#with git on a new computer
+git config --global user.name "Joshua Parker"
+git config --global user.email "jparker291@gmail.com"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
